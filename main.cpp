0fc15b908509504d844644f0b90901b55042d852
MUHAMMAD-7277272 <145957994+MUHAMMAD-7277272@users.noreply.github.com>
2024-01-21 04:44:40 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<iomanip>
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+using namespace std;
+
+
+int main()
+{
+
+    //1
+    // int a=71;
+    // int b=0x47;
+    // int c=0107;
+    // int d=0b1000111;
+
+    // cout<<"decimal : "<<a<<endl;
+    // cout<<"hex to decimal : "<<dec<<b<<endl;
+    // cout<<"oct to decimal : "<<dec<<c<<endl;
+    // cout<<"bin to decimal : "<<dec<<d<<endl;
+
+
+    //2
+    int n{2};
+    cout<<n<<endl;
+    cout<<setprecision(7);
+    double number=234686565.45739373653;
+    cout<<number<<endl;
+
+
+
+
+
+    return (sizeof(int));
+
+}
